910f424cba2310de6926cc513a2a16aac468c36b
4orrest <76661750+4orrest@users.noreply.github.com>
2023-05-30 10:55:17 -0700

    created functionality for adding and removing products, an empty cart state and added google fonts

--- app/components/Cart.tsx
+++ app/components/Cart.tsx
@@ -3,6 +3,8 @@
 import Image from "next/image"
 import { useCartStore } from '@/store'
 import formatPrice from "@/util/PriceFormat"
+import {IoAddCircle, IoRemoveCircle} from 'react-icons/io5'
+import basket from '@/public/empty-cart.png'
 
 export default function Cart(){
     const cartStore = useCartStore()
@@ -16,12 +18,38 @@ export default function Cart(){
                     <Image className="rounded-md h-24" src={item.image} alt={item.name} width={120} height={120}/>
                         <div>
                             <h2>{item.name}</h2>
+                            {/* UPDATE QUANTITY OF A PRODUCT */}
+                            <div className="flex gap-2">
                             <h2>Quantity: {item.quantity}</h2>
+                            <button onClick={() => cartStore.removeProduct({
+                                 id: item.id, 
+                                 image: item.image, 
+                                 name: item.name, 
+                                 price: item.price, 
+                                 quantity: item.quantity,
+                            })}><IoRemoveCircle /></button>
+                            <button onClick={() => cartStore.addProduct({
+                                id: item.id, 
+                                image: item.image, 
+                                name: item.name, 
+                                price: item.price, 
+                                quantity: item.quantity,})}><IoAddCircle /></button>
+                            </div>
                             <p className="text-sm">{item.price && formatPrice(item.price)}</p>
                         </div>
                     </div>
                 ))}
-                <button className="py-2 mt-4 bg-teal-700 w-full rounded-md text-white">Checkout</button>
+                {cartStore.cart.length > 0 && (
+                <button className="py-2 mt-4 bg-teal-700 w-full rounded-md text-white">
+                    Checkout
+                </button>
+                 )}
+                 {!cartStore.cart.length && (
+                    <div className="flex flex-col items-center gap-12 text-2xl font-medium pt-56 opacity-75">
+                        <h1>Uhh..It's empty! 🥲</h1>
+                        <Image src={basket} alt="empty cart" width={300} height={300}/>
+                    </div>
+                 )}
             </div>
         </div>
     )

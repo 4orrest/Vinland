71713343bf7f0d9d1fb79e0884e9675819a5effd
4orrest <76661750+4orrest@users.noreply.github.com>
2023-06-16 23:29:53 -0700

    added button design on OrderConfirmation screen

--- app/components/OrderConfirmed.tsx
+++ app/components/OrderConfirmed.tsx
@@ -1,42 +1,46 @@
-'use client'
+"use client";
 
-import {motion} from 'framer-motion'
-import Image from 'next/image'
-import dancing from '@/public/dancing.gif'
-import Link from 'next/link'
-import { useCartStore } from '@/store'
-import {useEffect} from 'react'
+import { motion } from "framer-motion";
+import Image from "next/image";
+import dancing from "@/public/dancing.gif";
+import Link from "next/link";
+import { useCartStore } from "@/store";
+import { useEffect } from "react";
 
-export default function OrderConfirmed(){
-    const cartStore = useCartStore()
-    useEffect(() => {
-        cartStore.setPaymentIntent('')
-        cartStore.clearCart()
-    }, [])
+export default function OrderConfirmed() {
+  const cartStore = useCartStore();
+  useEffect(() => {
+    cartStore.setPaymentIntent("");
+    cartStore.clearCart();
+  }, []);
 
-    const checkoutOrder = () => {
-        setTimeout(() => {
-            cartStore.setCheckout('cart')
-        }, 1000)
-        cartStore.toggleCart()
-    }
-    return(
-        <motion.div
-        className='flex items-center justify-center my-12' 
-        initial={{ scale: 0.5, opacity: 0 }}
-        animate={{scale: 1, opacity: 1 }}>
-            <div className='p-12 rounded-md text-center'>
-                <h1 className='text-2xl font-medium'>Your order has been placed 🚀</h1>
-                <h2 className='text-sm my-4'>Check your email for the receipt</h2>
-                <Image src={dancing} className='py-8' alt='dancing kid'/>
-                <div className='flex items-center justify-center gap-12'>
-                    <Link href={'/dashboard'}>
-                        <button onClick={checkoutOrder} 
-                        className='font-medium'
-                        >Check your Order</button>                
-                    </Link>
-                </div>
-            </div>
-        </motion.div>
-    )
+  const checkoutOrder = () => {
+    setTimeout(() => {
+      cartStore.setCheckout("cart");
+    }, 1000);
+    cartStore.toggleCart();
+  };
+  return (
+    <motion.div
+      className="flex items-center justify-center my-12"
+      initial={{ scale: 0.5, opacity: 0 }}
+      animate={{ scale: 1, opacity: 1 }}
+    >
+      <div className="p-12 rounded-md text-center">
+        <h1 className="text-2xl font-medium">Your order has been placed 🚀</h1>
+        <h2 className="text-sm my-4">Check your email for the receipt</h2>
+        <Image src={dancing} className="py-8" alt="dancing kid" />
+        <div className="flex items-center justify-center gap-12">
+          <Link href={"/dashboard"}>
+            <button
+              onClick={checkoutOrder}
+              className="my-4 btn btn-primary w-full"
+            >
+              Check your Order
+            </button>
+          </Link>
+        </div>
+      </div>
+    </motion.div>
+  );
 }
